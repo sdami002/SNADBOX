b3c748a0e94a62aafe5ed12a4f2f6ab3b24182b3
sdami002 <123014790+sdami002@users.noreply.github.com>
2024-01-31 17:47:55 -0500

    Add files via upload

---
+++ src/components/Transactions/TransactionPane.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react"
+import { InputCheckbox } from "../InputCheckbox"
+import { TransactionPaneComponent } from "./types"
+
+export const TransactionPane: TransactionPaneComponent = ({
+  transaction,
+  loading,
+  setTransactionApproval: consumerSetTransactionApproval,
+}) => {
+  const [approved, setApproved] = useState(() => {
+    const savedApproved = localStorage.getItem(`transaction-${transaction.id}-approved`);
+    return savedApproved ? JSON.parse(savedApproved) : transaction.approved;
+  });  
+  useEffect(() => {
+    localStorage.setItem(`transaction-${transaction.id}-approved`, JSON.stringify(approved));
+  }, [approved, transaction.id]);
+  return (
+    <div className="RampPane">
+      <div className="RampPane--content">
+        <p className="RampText">{transaction.merchant} </p>
+        <b>{moneyFormatter.format(transaction.amount)}</b>
+        <p className="RampText--hushed RampText--s">
+          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+        </p>
+      </div>
+      <InputCheckbox
+        id={transaction.id}
+        checked={approved}
+        disabled={loading}
+        onChange={async (newValue) => {
+          await consumerSetTransactionApproval({
+            transactionId: transaction.id,
+            newValue,
+          })
+
+          setApproved(newValue)
+        }}
+      />
+    </div>
+  )
+}
+
+const moneyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})
